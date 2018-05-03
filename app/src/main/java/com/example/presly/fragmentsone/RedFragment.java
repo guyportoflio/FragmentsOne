fc70ce578753c4834b9b0bd7b89ddbe460274816
presly <guyportfolio32.gmail.com>
2018-05-03 03:23:32 +0300

    just a basic static fragments test

---
+++ app/src/main/java/com/example/presly/fragmentsone/RedFragment.java
@@ -0,0 +1,33 @@
+package com.example.presly.fragmentsone;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RedFragment extends Fragment {
+
+    // inflater = used to inflate the Fragment's layout.
+    // container = the containing layout of the activity.
+    // saveInstanceState = used to save / restore data.
+
+
+    public RedFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_red, container, false);
+    }
+
+}
